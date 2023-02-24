47af662ea02cda4bd35053a03edb67c6685dd962
Ahnafjb <63702813+Ahnafjb@users.noreply.github.com>
2023-02-24 17:23:10 +0600

    added data

---
+++ pphotodata.php
@@ -0,0 +1,76 @@
+
+
+{
+	"error": "false",
+	"message": "Get Recent All Stickers Category Wise List..",
+	"sticker": [
+		{
+			"sticat_id": "1",
+			"sticat_name": "Happy Birthday",
+			"stickers": [
+				{
+					"stisubcat_id": "1",
+					"sticat_id": "1",
+					"stisubcat_name": "1_Happy_birthday",
+					"stickers_img": [
+						{
+							"image_file": "https://pphoto.epizy.com/bgremover/bgimg/1_Happy_birthday/icon.jpg",
+							"url_front": "https://pphoto.epizy.com/bgremover/bgimg/1_Happy_birthday/front.png",
+							"url_back": "https://pphoto.epizy.com/bgremover/bgimg/1_Happy_birthday/back.png"
+						}
+					]
+				},
+				{
+					"stisubcat_id": "2",
+					"sticat_id": "1",
+					"stisubcat_name": "2_Happy_birthday",
+					"stickers_img": [
+						{
+							"image_file": "https://pphoto.epizy.com/bgremover/bgimg/2_Happy_birthday/icon.jpg",
+							"url_front": "https://pphoto.epizy.com/bgremover/bgimg/2_Happy_birthday/front.png",
+							"url_back": "https://pphoto.epizy.com/bgremover/bgimg/2_Happy_birthday/back.png"
+						}
+					]
+				},
+				{
+					"stisubcat_id": "3",
+					"sticat_id": "1",
+					"stisubcat_name": "3_Happy_birthday",
+					"stickers_img": [
+						{
+							"image_file": "https://pphoto.epizy.com/bgremover/bgimg/3_Happy_birthday/icon.jpg",
+							"url_front": "https://pphoto.epizy.com/bgremover/bgimg/3_Happy_birthday/front.png",
+							"url_back": "https://pphoto.epizy.com/bgremover/bgimg/3_Happy_birthday/back.png"
+						}
+					]
+				},
+				{
+					"stisubcat_id": "4",
+					"sticat_id": "1",
+					"stisubcat_name": "4_Happy_birthday",
+					"stickers_img": [
+						{
+							"image_file": "https://pphoto.epizy.com/bgremover/bgimg/4_Happy_birthday/icon.jpg",
+							"url_front": "https://pphoto.epizy.com/bgremover/bgimg/4_Happy_birthday/front.png",
+							"url_back": "https://pphoto.epizy.com/bgremover/bgimg/4_Happy_birthday/back.png"
+						}
+					]
+				},
+				{
+					"stisubcat_id": "5",
+					"sticat_id": "1",
+					"stisubcat_name": "5_Happy_birthday",
+					"stickers_img": [
+						{
+							"image_file": "https://pphoto.epizy.com/bgremover/bgimg/5_Happy_birthday/icon.jpg",
+							"url_front": "https://pphoto.epizy.com/bgremover/bgimg/5_Happy_birthday/front.png",
+							"url_back": "https://pphoto.epizy.com/bgremover/bgimg/5_Happy_birthday/back.png"
+						}
+					]
+				}
+			]
+		}
+		
+	]
+}
+
